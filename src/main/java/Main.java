6d94187937b91c425fe03cfa63d2c16714daa6f4
崔晨 <chen.cui@albus.net.cn>
2019-07-23 00:41:50 +0800

    d

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
@@ -13,40 +14,45 @@ import java.net.URLEncoder;
 
 public class Main {
 
-    static String getApplyInfo(){
-        String content = "{" +
-                "\"firstNameEnglish\": \"Xiu Lian\", " +
-                "\"lastNameEnglish\": \"Lee\", " +
-                "\"firstNameChinese\": \"秀莲\", " +
-                "\"lastNameChinese\": \"李\", " +
-                "\"formerFirstNameEnglish\": \"Xiu Lian\", " +
-                "\"formerLastNameEnglish\": \"Li\", " +
-                "\"formerFirstNameChinese\": \"绣莲\", " +
-                "\"formerLastNameChinese\": \"李\", " +
-                "\"gender\": \"Female\",\n" +
-                "\"dateOfBirth\": \"1980-12-31\", " +
-                "\"placeOfBirth\": \"Shenzhen\", " +
-                "\"maritalStatus\": \"Married\", " +
-                "\"identityNumber\": \"622825199303180010\"," +
-                "\"nationality\": \"Chinese\",\n" +
-                "\"ethnicity\": \"Han\",\n" +
-                "\"ancestralHome\": \"Suzhou\",\n" +
-                "\"domicile\": \"1881 Bao'an S Rd, CaiWuWei, Luohu Qu, Shenzhen Shi, Guangdong Sheng,\n" +
-                "China, 518000\",\n" +
-                "\"photoUrl\": \"\", " +
-                "\"passport\": {\n" +
-                "\"number\": \"G05473471\",\n" +
-                "\"type\": \"P - Ordinary\",\n" +
-                "\"placeOfIssue\": \"Beijing\", \"pictureUrl\": \"\"\n" +
-                "}, " +
-                "\"visa\": {\n" +
-                "\"category\": \"Tourist Visa\",\n" +
-                " \"type\": \"Single Entry Visa\"\n" +
-                "}\n" +
-                "},\n" +
-                "\"phoneNumber\": \"+86 13522568027\", " +
-                "\"email\": \"xiulian@qq.com\"";
-        return content;
+    static String getApplyInfo(String token){
+        String params = "{\n" +
+                "  \"client_id\": \"123456\",\n" +
+                "  \"access_token\": \"" + token + "\",\n" +
+                "  \"title\": \"test\",\n" +
+                "  \"content\": {\n" +
+                "    \"firstNameEnglish\": \"Xiu Lian\",\n" +
+                "    \"lastNameEnglish\": \"Lee\",\n" +
+                "    \"firstNameChinese\": \"秀莲\",\n" +
+                "    \"lastNameChinese\": \"李\",\n" +
+                "    \"formerFirstNameEnglish\": \"Xiu Lian\",\n" +
+                "    \"formerLastNameEnglish\": \"Li\",\n" +
+                "    \"formerFirstNameChinese\": \"绣莲\",\n" +
+                "    \"formerLastNameChinese\": \"李\",\n" +
+                "    \"gender\": \"Female\",\n" +
+                "    \"dateOfBirth\": \"1980-12-31\",\n" +
+                "    \"placeOfBirth\": \"Shenzhen\",\n" +
+                "    \"maritalStatus\": \"Married\",\n" +
+                "    \"identityNumber\": \"622825199303180010\",\n" +
+                "    \"nationality\": \"Chinese\",\n" +
+                "    \"ethnicity\": \"Han\",\n" +
+                "    \"ancestralHome\": \"Suzhou\",\n" +
+                "    \"domicile\": \"1881 Bao'an S Rd, CaiWuWei, Luohu Qu, Shenzhen Shi, Guangdong Sheng, China, 518000\",\n" +
+                "    \"photoUrl\": \"\",\n" +
+                "    \"passport\": {\n" +
+                "      \"number\": \"G05473471\",\n" +
+                "      \"type\": \"P - Ordinary\",\n" +
+                "      \"placeOfIssue\": \"Beijing\",\n" +
+                "      \"pictureUrl\": \"\"\n" +
+                "    },\n" +
+                "    \"visa\": {\n" +
+                "      \"category\": \"Tourist Visa\",\n" +
+                "      \"type\": \"Single Entry Visa\"\n" +
+                "    },\n" +
+                "    \"phoneNumber\": \"+86 13522568027\",\n" +
+                "    \"email\": \"xiulian@qq.com\"\n" +
+                "  }\n" +
+                "}";
+        return params;
     }
 
     public static void main(String[] args) {
@@ -55,10 +61,11 @@ public class Main {
         System.out.println(token.getAccess_token());
         String result = applicants(token.getAccess_token());
         System.out.println(result);
+//        System.out.println(getToken());
     }
 
     private static String getToken() {
-        String url = "http://39.108.144.152:8100/api/token?client_id=123456&client_secret=123abc";
+        String url = "http://127.0.0.1:8100/api/token.json?client_id=123456&client_secret=123abc";
         CloseableHttpClient client = HttpClients.createDefault();
         HttpGet get = new HttpGet(url);
         get.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0");
@@ -72,18 +79,15 @@ public class Main {
     }
 
     private static String applicants(String token) {
-        String content = getApplyInfo();
-        String url = "http://39.108.144.152:8100/api/applicants?client_id=123456&access_token=" + token + "&title=test&content=" + content;
-        String u = null;
-        try {
-            u = URLEncoder.encode(url,"UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        url = u.replace("%3F", "?").replace("%26", "&").replace("%3D", "=").replace("%2F", "/").replace("%3A", ":").replace("%25","%").replace("%2B","+").replace("%2C",",");
+        String params = getApplyInfo(token);
+        String url = "http://127.0.0.1:8100/api/applicants.json";
         CloseableHttpClient client = HttpClients.createDefault();
         HttpPost post = new HttpPost(url);
+        StringEntity requestEntity = new StringEntity(params,"utf-8");
+        requestEntity.setContentEncoding("UTF-8");
+        post.setHeader("Content-type", "application/json");
         post.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0");
+        post.setEntity(requestEntity);
         try {
             CloseableHttpResponse response = client.execute(post);
             return IOUtils.toString(response.getEntity().getContent());
